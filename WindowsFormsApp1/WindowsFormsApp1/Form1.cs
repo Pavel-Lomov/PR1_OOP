2de6323f67a665d01bf68086b9212327636bdecf
Admin <LPN1972@yandex.ru>
2020-09-17 10:18:59 +1000

    PR 1 variant 9

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double X1 = Convert.ToDouble(textBox2.Text);
+            double Y1 = Convert.ToDouble(textBox3.Text);
+            double X2 = Convert.ToDouble(textBox4.Text);
+            double b = Y1 - a * X1;
+            double Y2 = a * X2 + b;
+            textBox5.Text = Y2.ToString();
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_BackColorChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int C1 = Convert.ToInt32(textBox1.Text);
+            int n = Convert.ToInt32(textBox2.Text);
+            int C2 = Convert.ToInt32(textBox3.Text);
+            int c = n * 100 / C1;
+            int f = C2 - C2 * c / 100;
+            textBox4.Text = f.ToString();
+            textBox5.Text = c.ToString();
+        }
+
+    }
+}
