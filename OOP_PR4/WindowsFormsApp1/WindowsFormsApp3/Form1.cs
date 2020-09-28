75873c6f17b67c6f2be890a023c97b03857bf1d3
Admin <LPN1972@yandex.ru>
2020-09-28 17:39:33 +1000

    OOP_PR4

---
+++ OOP_PR4/WindowsFormsApp1/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+       public static int progress = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progress += 1;
+            if (progress>101)
+            {
+                progress = 0;
+            }
+            progressBar1.Value = progress;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
