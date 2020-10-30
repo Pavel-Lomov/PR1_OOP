0ea1b91536d200432c49c1e618df12e423a6ea85
Pavel-Lomov <71361248+Pavel-Lomov@users.noreply.github.com>
2020-10-30 20:52:13 +1000

    Add files via upload

---
+++ OOP_PR7/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox3.Text);
+            double b = Convert.ToDouble(textBox4.Text);
+            string name_tovar = textBox2.Text;
+            string name_ctrana = textBox1.Text;
+            Tovar d = new Tovar(name_tovar,name_ctrana, a, b);
+            listBox1.Items.Add(d);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OOP_PR7/WindowsFormsApp1/WindowsFormsApp1/Tovar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Tovar
+    {
+        public string name_tovar, name_ctrana;
+        public int a;
+        public double b;
+
+        public Tovar()
+        {
+            name_ctrana = "";
+            name_tovar = "";
+            a = 0;//Объем годового импорта
+            b = 0;//Стоимость единицы товара
+        }
+        public Tovar(string new_ctrana,string new_tovar, int new_a,double new_b)
+        {
+            name_tovar = new_tovar;
+            name_ctrana = new_ctrana;
+            a = new_a;
+            b = new_b;
+        }
+        public override string ToString()
+        {
+            return "Товар - "+ name_tovar.ToString()+";"+"  Страна - " + name_ctrana.ToString()+";"+
+                "   Объем за год - " + a.ToString()+";"+"   Стоимость ед. - "+ b.ToString();
+
+        }
+        
+    }
+}
